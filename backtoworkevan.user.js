eedda8f40bef502354154ea528d3f321121f67e8
Brad Mallow <briznad@gmail.com>
2013-04-22 16:11:23 -0300

    added whitelist

--- backtoworkevan.user.js
+++ backtoworkevan.user.js
@@ -9,11 +9,16 @@
 // do stuff
 
 // new date object
-var d = new Date();
+var d = new Date(),
+  whitelist = [
+		'/groups/lyftdrivers/',
+		'/groups/lyftdriversla/',
+		'/groups/lyftdriversseatle/'
+	];
 
 // conditionals:
 // if not weekend (sat or sun)
 // if in work hours (9-6)
-// if not https://www.facebook.com/groups/lyftdrivers/
+// if current page is not whitelisted
 // then redirect away from facebook
-if (!(d.getDay() === 6 || d.getDay() === 0) && (d.getHours() > 8 && d.getHours() < 17) && !(window.location.pathname ===  '/groups/lyftdrivers/')) window.location = 'http://briznad.github.io/backToWorkEvan/';
+if (!(d.getDay() === 6 || d.getDay() === 0) && (d.getHours() > 8 && d.getHours() < 17) && whitelist.indexOf(window.location.pathname) ===  -1) window.location = 'http://briznad.github.io/backToWorkEvan/';
